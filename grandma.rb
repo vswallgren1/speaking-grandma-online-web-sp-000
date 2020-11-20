122db99020cc77b3ee2107786d2e2b355740bc35
Vince W <vincewallgren@me.com>
2020-11-20 05:16:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,11 @@
-speak_to_grandma = gets.chomp
-
-if speak_to_grandma == "Hi Nana, how are you?"
-  puts "HUH?! SPEAK UP, SONNY"
-elsif speak_to_grandma == "Hi!"
-  puts "HUH?! SPEAK UP, SONNY"
+def speak_to_grandma(speak)
+  if speak == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif speak == speak.upcase
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
