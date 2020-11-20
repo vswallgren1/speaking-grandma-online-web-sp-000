f0ba813308ef3c1f7a44a28b10698dc156ecbff2
Vince W <vincewallgren@me.com>
2020-11-20 05:05:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ speak_to_grandma = gets.chomp
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
